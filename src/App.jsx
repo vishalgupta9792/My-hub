b06222f2cbd9f5c4209e6dfa6a742219c856687d
Your Name <your-email@example.com>
2026-02-10 15:13:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hello from "./Hello";
+import Button from "./Button";
+import './App.css';
+
+function App() {
+ 
+  return (
+    <div>
+      <h1>My-Hub</h1>
+      <Hello />
+      <Button />
+    </div>
+  );
+}
+
+export default App
